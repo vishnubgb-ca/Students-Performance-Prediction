78b57196140fc4c45c328cedc8b4e2d49e849117
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2023-10-11 12:13:04 +0530

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -1,25 +1,15 @@
 from loading_data import loading_data
 
 def data_analysis():
-    data1, data2 = loading_data()
-    print(data1.info())
-    print(data2.info())
-    print(data1.describe())
-    print(data2.describe())
-    print(data1.columns)
-    print(data1.shape)
-    print(data1.head())
-    print(data1.tail())
-    print(data2.columns)
-    print(data2.shape)
-    print(data2.head())
-    print(data2.tail())
-    print(data1.isnull().sum())
-    print(data2.isnull().sum())
-    for col in data1.columns:
-        print(col, data1[col].nunique())
-    for col in data2.columns:
-        print(col, data2[col].nunique())
-    return data1, data2
+    data = loading_data()
+    print (data.head())
+    print (data.tail())
+    print (data.describe())
+    print ("Rows     : " ,data.shape[0])
+    print ("Columns  : \n" ,data.shape[1])
+    print ("Features : \n" ,data.columns.tolist())
+    print ("Missing values\n\n",data.isnull().any(),sep='')
+    print ("Unique values\n\n",data.nunique(),sep='')
+    return data
 
 data_analysis()
