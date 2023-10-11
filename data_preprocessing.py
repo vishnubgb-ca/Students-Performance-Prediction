59843955702dc415a4535269b18fb817ecac2dc4
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2023-10-11 12:14:36 +0530

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,15 +2,8 @@ import pandas as pd
 from loading_data import loading_data
 
 def data_preprocess():
-    data1, data2 = loading_data()
-    whole_df = pd.merge(data2, data1, on='DeviceTimeStamp')
-    whole_df = whole_df.drop(['DeviceTimeStamp'], axis=1)
-    # whole_df.drop(['OTI_A','OTI_T'], axis=1,inplace=True)
-    print(whole_df)
-    print(whole_df.duplicated())
-    print(whole_df.isnull().sum())
-    whole_df['MOG_A'] = whole_df['MOG_A'].astype('int')
-    whole_df.columns = whole_df.columns.str.replace(' ', '_') 
-    return whole_df
+    data = loading_data()
+    print(data)
+    return data
 
 data_preprocess()
