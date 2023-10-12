53749564ea7676a02791d9c88291b8baa9f88b3c
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2023-10-12 12:59:14 +0530

    Update datavisualization.py

--- datavisualization.py
+++ datavisualization.py
@@ -19,6 +19,10 @@ def data_visualization():
         sns.distplot(x=data[i])
         plt.title(i)
     plt.show()
+    df=data.drop("Extracurricular Activities",axis=1)
+    plt.figure(figsize=(10,8))
+    sns.heatmap(df.corr(), linewidth=1, annot=True,  cmap="coolwarm", fmt=".4f")
+    plt.show()
     return data
 
 data_visualization()
