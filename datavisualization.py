1628b4c789f5e7f58b26e39ea825ff740d7d64f0
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2023-11-02 15:15:05 +0530

    Update datavisualization.py

--- datavisualization.py
+++ datavisualization.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from data_preprocessing import data_preprocess
+import plotly.figure_factory as ff
+import plotly.express as px
 
 def data_visualization():
 
@@ -11,18 +13,23 @@ def data_visualization():
     col.remove("Extracurricular Activities")
     print(col)
     for i in col:
-        plt.figure(figsize=(14,12))
-        sns.boxplot(y=i,data=data)
-    plt.show()
+        fig = px.box(data, y=i)
+        fig.update_layout(plot_bgcolor = "black")
+        fig.update_xaxes(showgrid=False,zeroline=False)
+        fig.update_yaxes(showgrid=False,zeroline=False)
+        fig.show()
     for i in col:
-        plt.figure(figsize=(14,12))
-        sns.distplot(x=data[i])
-        plt.title(i)
-    plt.show()
+        fig = ff.create_distplot([data[i].values],group_labels=[i])
+        fig.update_layout(plot_bgcolor = "black")
+        fig.update_xaxes(showgrid=False,zeroline=False)
+        fig.update_yaxes(showgrid=False,zeroline=False)
+        fig.show()
     df=data.drop("Extracurricular Activities",axis=1)
-    plt.figure(figsize=(14,12))
-    sns.heatmap(df.corr(), linewidth=1, annot=True,  cmap="coolwarm", fmt=".4f")
-    plt.show()
+    y=df.corr().columns.tolist()
+    z=df.corr().values.tolist()
+    z_text = np.around(z, decimals=4) # Only show rounded value (full value on hover)
+    fig = ff.create_annotated_heatmap(z,x=y,y=y,annotation_text=z_text,colorscale=px.colors.sequential.Cividis_r,showscale=True)
+    fig.show()
     return data
 
 data_visualization()
